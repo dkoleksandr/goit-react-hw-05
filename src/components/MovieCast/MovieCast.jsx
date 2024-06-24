80752f121c9ea09ea7b4db02453d3cd81d7152a4
dkoleksandr <didenk.oleksandr@gmail.com>
2024-06-24 11:39:45 +0300

    fix after review

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -7,7 +7,7 @@ import css from "./MovieCast.module.css";
 
 const POSTER_URL = "https://image.tmdb.org/t/p/w500";
 const defaultImg =
-  "<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>";
+  "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
 
 const MovieCast = () => {
   const [movieCasts, setMovieCasts] = useState([]);
